9752dac33ead7cb690113cb5e2b26f1ce417df64
Vishal917978 <vishalsharma917978@gmail.com>
2025-10-10 15:09:51 +0530

    something do

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -1,47 +1,48 @@
 import React, { useState } from "react";
 
-function regiteruser(){
-let[name,Setname]=useState("");
-let[email,Setemail]=useState("");
-let[password,Setpassword]=useState("")
+function regiteruser() {
+    let [name, Setname] = useState("");
+    let [email, Setemail] = useState("");
+    let [password, Setpassword] = useState("")
 
-function handleSubmit(){
+    function handleSubmit() {
 
-}
-    return(
+    }
+    return (
         <>
-        <form onSubmit={handleSubmit}>
-        <label>Username</label>
+            <form onSubmit={handleSubmit}>
+                <label>Username</label>
 
-        <input
-         type="text"
-         placeholder="Enter your name" 
-         onChange={(e)=>Setname(e.target.value)} 
-         value={name}
-         />
+                <input
+                    type="text"
+                    placeholder="Enter your name"
+                    onChange={(e) => Setname(e.target.value)}
+                    value={name}
+                />
 
-        <label>Email</label>
-        <input
-        type="email" 
-        placeholder="Enter your Email"
-        onChange={(e)=>Setemail(e.target.value)} 
-        value={email}
-        />
+                <label>Email</label>
+                <input
+                    type="email"
+                    placeholder="Enter your Email"
+                    onChange={(e) => Setemail(e.target.value)}
+                    value={email}
+                />
 
-        <label>Password</label>
+                <label>Password</label>
 
-        <input 
-        type="text"
-        placeholder="Enter your password"
-        onChange={(e)=>Setpassword(e.target.value)} 
-        value={password}
-        />
+                <input
+                    type="text"
+                    placeholder="Enter your password"
+                    onChange={(e) => Setpassword(e.target.value)}
+                    value={password}
+                />
 
-        <button
-        type="submit">
-        Submit
-        </button>
-        </form>
+                <button
+                    type="submit">
+                    Submit
+                </button>
+            </form>
         </>
     )
 }
+export default regiteruser;
